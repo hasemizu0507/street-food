48a5439cc14ddff60718fc3729aa80d109611c6d
hasemizu0507 <hasemizu0507@ezweb.ne.jp>
2021-08-30 02:03:51 +0900

    2021.08.30.2:03

--- streetfood_proj/streetfood/views/concept.py
+++ streetfood_proj/streetfood/views/concept.py
@@ -8,12 +8,4 @@ from streetfood.models import Food, Profile, Cart
 
 
 def concept_top(request):
-    user = request.user
-    profile = user.profile
-    user_id = user.id
-    # owner_id=profile.idの商品を取り出し、テンプレートに渡す。
-    myfoods = Food.objects.filter(owner_id=profile.id)
-    # ユーザーIDがログイン中しているユーザのIDになっているproductsを取り出す。
-    products = Cart.objects.filter(user_id=user_id)
-
-    return render(request, 'streetfood/concept.html', {'profile':profile,'foods':myfoods, 'products': products}) #'assessments':myassessments
+    return render(request, 'streetfood/concept.html') #'assessments':myassessments
